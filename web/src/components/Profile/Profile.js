b63b1c425e29565d2fbe1c42c821be9259642ea6
rohitAniLabs <rohit@anilabsinc.com>
2023-04-19 15:54:59 +0530

    profile-page

--- web/src/components/Profile/Profile.js
+++ web/src/components/Profile/Profile.js
@@ -1,17 +1,17 @@
 import { useAuth } from "src/auth";
 
 const Profile = () => {
-  const {isAuthenticated} = useAuth()
+  const {isAuthenticated,userMetadata} = useAuth()
   if(!isAuthenticated){
     return(<div>Loading...</div>)
   }
-  console.log({useAuth});
+  console.log({userMetadata})
   return (
     
     <div>
-      {/* <h1>{user}</h1> */}
-      {/* <p>Email:{currentUser.email}</p>
-      <img src ={currentUser.picture} alt="User Avtar"/> */}
+      <h1>{userMetadata.name}</h1>
+      <p>Email:{userMetadata.email}</p>
+      <img src ={userMetadata.picture} alt="User Avtar"/>
     </div>
   )
 }
