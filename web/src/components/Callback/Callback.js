149ed1987354344df1327fcd797ce4b4454ef7f5
rohitAniLabs <rohit@anilabsinc.com>
2023-04-19 18:07:09 +0530

    added login with popup

--- web/src/components/Callback/Callback.js
+++ web/src/components/Callback/Callback.js
@@ -1,13 +1,12 @@
 import { useAuth } from "src/auth";
 import { useEffect } from "react";
 import { navigate } from '@redwoodjs/router'
-import Profile from "../Profile/Profile";
 const Callback = () => {
-  const { logIn } = useAuth()
+  const {loginWithPopup} = useAuth()
 
   useEffect(() => {
     console.log('Auth0CallbackPage loaded')
-    logIn().then(() => {
+    loginWithPopup().then(() => {
       console.log('User logged in')
       navigate('/')
     }).catch((error) => {
