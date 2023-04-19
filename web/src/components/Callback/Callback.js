23b6b2da8bece8a8e9c782273796bf6e9ee1ed11
rohitAniLabs <rohit@anilabsinc.com>
2023-04-19 17:56:05 +0530

    added consoles

--- web/src/components/Callback/Callback.js
+++ web/src/components/Callback/Callback.js
@@ -6,8 +6,12 @@ const Callback = () => {
   const { logIn } = useAuth()
 
   useEffect(() => {
+    console.log('Auth0CallbackPage loaded')
     logIn().then(() => {
+      console.log('User logged in')
       navigate('/')
+    }).catch((error) => {
+      console.error('Error logging in:', error)
     })
   }, [logIn])
   return <div>Logging in...</div>
