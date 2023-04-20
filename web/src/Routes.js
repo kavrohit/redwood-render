be401ba341a4a59c479f9cdb6c75f3cd823fb3e4
rohitAniLabs <rohit@anilabsinc.com>
2023-04-20 14:31:57 +0530

    changed-routes

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,16 +1,14 @@
-
-import HomePage from './pages/HomePage/HomePage'
-
 import { Router, Route } from '@redwoodjs/router'
-import Profile from './components/Profile/Profile'
 
 import { useAuth } from './auth'
+// import Profile from './components/Profile/Profile'
+import HomePage from './pages/HomePage/HomePage'
 
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Route path= "/" page = {HomePage} name="home"/>
-      <Route path= "/profile" page = {Profile} name ="callback"/>
+      <Route path="/" page={HomePage} name="home" />
+      {/* <Route path= "/" page = {callback} name ="callback"/> */}
       <Route notfound page={NotFoundPage} />
     </Router>
   )
