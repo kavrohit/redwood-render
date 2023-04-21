76418d89f3df5f1e43210d394e00e1a0439e565a
rohitAniLabs <rohit@anilabsinc.com>
2023-04-21 15:33:50 +0530

    added-scopes

--- web/src/components/Profile/Profile.js
+++ web/src/components/Profile/Profile.js
@@ -3,14 +3,14 @@ import { useAuth } from 'src/auth'
 const Profile = () => {
   const { currentUser, userMetadata, loading } = useAuth()
   if (loading) {
-    return <div>Loading...</div>
+    return null
   }
   console.log({ userMetadata }, { currentUser })
   return (
     <div>
-      {/* <h1>{userMetadata.name}</h1>
+      <h1>{userMetadata.name}</h1>
       <p>Email:{userMetadata.email}</p>
-      <img src={userMetadata.picture} alt="User Avtar" /> */}
+      <img src={userMetadata.picture} alt="User Avtar" />
     </div>
   )
 }
